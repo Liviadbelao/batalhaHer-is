4419b337b31c88bbd2fd137efbb16bb4e718aaf0
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2024-05-06 08:41:23 -0300

    feat: create rota get da tabela herois

--- db/script.sql
+++ db/script.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE batalhaherois;
+
+
+CREATE TABLE herois (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    power VARCHAR(100) NOT NULL,
+    level INT NOT NULL, 
+    hp INT NOT NULL,
+    equipe VARCHAR(100),
+    editora VARCHAR(100)
+);
--- index.js
+++ index.js
@@ -18,7 +18,20 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('a rota esta funcionando');
 });
+// rota de todos os herois
 
+app.get('/herois', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM herois');
+        res.json({
+            total: resultado.rowCount,
+            herois: resultado.rows,
+        });
+    } catch (error) {
+        console.error('erro a obter todos os herois', error);
+        res.status(500).send('erro ao obter os herois');
+    }
+});
 app.listen(PORT, () => {
     console.log(`servidor rodando na porta ${PORT}⚡`);
 });
