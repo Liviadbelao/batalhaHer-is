a517e08fcfa54290d05598765ef66873cc60895e
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2024-05-06 09:02:28 -0300

    feat: create verificações de herois

--- index.js
+++ index.js
@@ -36,9 +36,21 @@ app.get('/herois', async (req, res) => {
 app.post('/herois', async (req, res) => {
     try {
         const { nome, power, level, hp, equipe, editora } = req.body;
+   if(!nome || !power || !level || !hp){
+    res.status(401).send({ mensagem: 'os campos : nome, power, level e hp, devem ser preenchidos!' });
 
-        await pool.query('INSERT INTO herois (nome, power, level, hp, equipe, editora) VALUES ($1, $2, $3, $4, $5, $6)', [nome, power, level, hp, equipe, editora]);
+   } else if (nome.length < 3){
+    res.status(401).send({ mensagem: 'o nome não pode ter menos que 3 letras' });
+   }
+    else if ( level < 0){
+    res.status(401).send({ mensagem: 'o level não pode ser menor que 0' });
+   }
+    else if ( hp < 0){
+    res.status(401).send({ mensagem: 'o hp não pode ser menor que 0' });
+   }
+     else{  await pool.query('INSERT INTO herois (nome, power, level, hp, equipe, editora) VALUES ($1, $2, $3, $4, $5, $6)', [nome, power, level, hp, equipe, editora]);
         res.status(201).send({ mensagem: 'heroi criado com sucesso' });
+}
     }
     catch (error) {
         console.error('erro ao inserir heroi', error);
@@ -64,9 +76,23 @@ app.put('/herois/:id', async (req, res) => {
         const { id } = req.params;
         const { nome, power, level, hp, equipe, editora } = req.body;
   
+        if(!nome || !power || !level || !hp){
+            res.status(401).send({ mensagem: 'os campos : nome, power, level e hp, devem ser preenchidos!' });
+        
+           } else if (nome.length < 3){
+            res.status(401).send({ mensagem: 'o nome não pode ter menos que 3 letras' });
+           }
+            else if ( level < 0){
+            res.status(401).send({ mensagem: 'o level não pode ser menor que 0' });
+           }
+            else if ( hp < 0){
+            res.status(401).send({ mensagem: 'o hp não pode ser menor que 0' });
+           }
+    
+           else{
             await pool.query('UPDATE herois SET nome = $1, power = $2, level = $3, hp = $4, equipe = $5, editora = $6 WHERE id = $7', [nome, power, level, hp, equipe, editora, id]);
             res.status(200).send({ mensagem: 'heroi atualizado' });
-        
+           }
     } catch (error) {
         console.error('erro ao atualizar heroi', error);
         res.status(500).send('erro ao atualizar heroi');
